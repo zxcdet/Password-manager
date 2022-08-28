2f24d79504e314095ce246171c136cbd41d35b03
admin <admin@admins-MacBook-Pro.local>
2022-08-28 13:58:57 +0300

    first commit

---
+++ src/app/card-password/card-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-password',
+  templateUrl: './card-password.component.html',
+  styleUrls: ['./card-password.component.css']
+})
+export class CardPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
